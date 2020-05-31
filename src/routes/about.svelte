09b371c2d050ad5a3c78ea212e68bf833230103b
Nick Moore <nickmoore_1@live.com>
2020-05-31 11:27:54 -0400

    Updating and stylling the site

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,48 @@
+<script>
+	let window_width;
+
+	function isMobile() {
+		console.log('Checking if it is mobile');
+	}
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<div id="main" class="relative flex w-screen h-screen mt-12">
+<svelte:window bind:innerWidth={window_width} on:resize={isMobile}/>
+
+<div id="main" class="relative flex flex-col w-screen h-screen mt-12">
 	<h1 class="w-full text-white text-center text-md md:text-4xl">Determination | Faith | Community | Lifestyle</h1>
+	<div id="about-main-info">
+		<p class="text-white">
+			Hey there! My name is Andrew Ingram and bicycles are my passion. I have been into bikes as long as I can remember. 
+			Through highschool and college I was lucky enough to travel around the country racing my mountain bike. 
+			When I wasn’t racing I was building trail or advocating publically for more trails to be built. 
+			Sometimes it meant scraping together a few dollars to race a national championship and sometimes it was just plain 
+			hard work in the dirt to have a new place to play. It was an awkward journey filled with many setbacks and injuries but the 
+			lessons I learned along the way have fueled my passion for bikes in a way that nothing else could. Hurricane Cycles 
+			was born as a means to share that passion. Since its humble inception in 2016 we have served hundreds of returning 
+			customers and helped to boost the growth of bike riding in the area in a way that would not have been possible otherwise. 
+			It has been an honor to serve this community and see this small business grow to the point that we can now give back in a very real way. 
+			Bringing the trails at Cumberland Mountain to life has been one of my greatest accomplishments. 
+			To see the many smiles and the families who are now getting outside and doing things together because of the trails is 
+			something that will stay with me for life. I moved back to Crossville because I love it here and it is my deepest honor to 
+			continue giving back and making my small town just a little bit better. Thank you to all of the amazing customers who have made this journey possible. 
+			Together we will continue making the world a better place, one bicycle at a time.
+		</p>
+	</div>
+	<div class="flex" id="video-container">
+		<iframe
+			class="mx-auto"
+			width="1020"
+			height="630"
+			src="https://www.youtube.com/embed/4_s1X0CRF4E"
+			title="Hurricane Video - 1"
+			frameborder="0"
+			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		>
+		</iframe>
+	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <div id="main" class="relative flex flex-col w-screen h-screen mt-12 text-center">
-	<h2 class="text-3xl p-4 w-3/4 mx-auto bg-white myDisplay-2 parell-gram">We don't just sell bikes...WE ARE LIFE-IMPROVEMENT SPECIALISTS</h2>
+	<h2 class="text-xl md:text-3xl p-4 w-3/4 mx-auto md:-mt-12 mb-4 bg-white myDisplay-2 rounded-lg">We don't just sell bikes...WE ARE LIFE-IMPROVEMENT SPECIALISTS</h2>
 	<div class="flex flex-row flex-wrap align-center justify-center md:p-4 bg-gray-900" id="contact-container">
 		<div id="contact" class="my-2 md:mx-4 md:my-0 h-auto">
 			<h1 class="text-center text-3xl md:text-5xl myDisplay-1">Contact</h1>
@@ -26,5 +26,13 @@
 			<p class="text-center text-white">Saturday: 10:00 AM - 5:00 PM</p>
 			<p class="text-center text-white">Sunday: <span class="text-xl myDisplay-1">Closed</span></p>
 		</div>
+	</div>
+	<div class="px-8 py-4" id="about-blurb">
+		<p class="text-white text-xl md:text-2xl"> We don’t sell bikes. Hurricane Cycles uses the humble bicycle as a means to improve the lives of our customers. 
+			We may have master certified bicycle mechanics on staff, but we are better described as life-improvement professionals. 
+			You see, most people don’t want a bicycle merely for the sake of owning a bicycle. Perhaps you fall into the group that 
+			simply wants to get from one place to another without a car and a bike sure beats walking. Or perhaps you are looking for 
+			the comradery that comes from riding bikes with friends. Some people enjoy the mental and physical challenge of mountain biking.
+		</p>
 	</div>
 </div>
